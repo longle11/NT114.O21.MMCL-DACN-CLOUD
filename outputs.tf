453717be9906e5d048cae333b9c09c9fdad5b7ce
longle11 <longleee2003@gmail.com>
2024-04-09 23:01:03 +0700

    delete output

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 # EC2  bastion outputs
-## ec2_bastion_public_instance_ids
-output "ec2_bastion_public_instance_ids" {
-  description = "List of IDs of instances"
-  value       = module.ec2_bastion_public.id
-}
+# ## ec2_bastion_public_instance_ids
+# output "ec2_bastion_public_instance_ids" {
+#   description = "List of IDs of instances"
+#   value       = module.ec2_bastion_public.id
+# }
 
-## ec2_bastion_public_ip
-output "ec2_bastion_eip" {
-  description = "Elastic IP associated to the Bastion Host"
-  value       = aws_eip.bastion_elastic_ip.public_ip
-}
+# ## ec2_bastion_public_ip
+# output "ec2_bastion_eip" {
+#   description = "Elastic IP associated to the Bastion Host"
+#   value       = aws_eip.bastion_elastic_ip.public_ip
+# }
 
 
 # Eks outputs
