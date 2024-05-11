e7e3c302c6c0ad1d14b50aa222bb98d1ee02d942
longle11 <longleee2003@gmail.com>
2024-05-11 16:56:26 +0700

    update new

--- prometheus.tf
+++ prometheus.tf
@@ -1,44 +1,35 @@
-resource "kubernetes_namespace" "kube-namespace" {
-  depends_on = [aws_eks_node_group.eks_nodegroup_private]
-  metadata {
-    name = "prometheus"
-  }
-}
-
-resource "helm_release" "prometheus" {
-  depends_on = [kubernetes_namespace.kube-namespace]
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  namespace  = kubernetes_namespace.kube-namespace.id
-  create_namespace = true
-  version    = "45.7.1"
-  values = [
-    file("./files/values.yaml")
-  ]
+# resource "helm_release" "prometheus" {
+#   depends_on = [aws_eks_node_group.eks_nodegroup_private]
+#   name       = "prometheus"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#   namespace  = "kube-system"   
+#   values = [
+#     file("./files/values.yaml")
+#   ]
   
 
-set {
-    name  = "podSecurityPolicy.enabled"
-    value = true
-  }
+# set {
+#     name  = "podSecurityPolicy.enabled"
+#     value = true
+#   }
 
-  set {
-    name  = "server.persistentVolume.enabled"
-    value = false
-  }
+#   set {
+#     name  = "server.persistentVolume.enabled"
+#     value = false
+#   }
 
-  set {
-    name = "server\\.resources"
-    value = yamlencode({
-      limits = {
-        cpu    = "200m"
-        memory = "50Mi"
-      }
-      requests = {
-        cpu    = "100m"
-        memory = "30Mi"
-      }
-    })
-  }
-}
+#   set {
+#     name = "server\\.resources"
+#     value = yamlencode({
+#       limits = {
+#         cpu    = "200m"
+#         memory = "50Mi"
+#       }
+#       requests = {
+#         cpu    = "100m"
+#         memory = "30Mi"
+#       }
+#     })
+#   }
+# }
